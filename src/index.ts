b577a1d746aab4aa684fed9c7d82ce7e8603a910
shinji5761 <shinji.tec.5761@gmail.com>
2023-07-30 11:47:17 +0900

    export の追加

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-
-export { default as Button } from './stories/forms/button/button';
+// === Common ===
+export { default as Badge } from './stories/badge/badge';
 export { default as Breadcrumb } from './stories/breadcrumb/breadcrumb';
+export { default as Modal } from './stories/modal/modal';
+export { default as Pager } from './stories/pager/pager';
+
+// === Form ===
+export { default as Avatar } from './stories/forms/avatar/avatar';
+export { default as Button } from './stories/forms/button/button';
+export { default as Checkbox } from './stories/forms/checkbox/checkbox';
+export { default as IconButton } from './stories/forms/icon.button/icon.button';
+export { default as Input } from './stories/forms/input/input';
+export { default as Selector } from './stories/forms/selector/selector';
+export { default as Textarea } from './stories/forms/textarea/textarea';
+
+// Skelton
+export * from './stories/skelton/skelton';
